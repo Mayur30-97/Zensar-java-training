3b3970617e60d15472856a66305d306cd9efb924
Mayur Ambalgi <m.ambalgi@zensar.com>
2022-04-22 13:25:06 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,17 @@
+
+
+public class first {
+    public static void main(String[] args) 
+    {
+        double b=50.9;
+        int a=30;
+
+        System.out.println("sum=" + (a+b));
+        System.out.println("subtration=" + (a-b));
+        System.out.println("multiplication=" + (a*b));
+        System.out.println("Division="+ (a/b));
+        
+    }
+
+    
+}
