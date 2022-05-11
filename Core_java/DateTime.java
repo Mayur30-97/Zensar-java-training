3b3dc1e8ce992bba19305c6b80aa3c5a69495395
Mayur30-97 <m.ambalgi@zensar.com>
2022-05-11 09:18:46 +0530

    Merge branch 'master' of https://github.com/Mayur30-97/Zensar-java-training

---
+++ Core_java/DateTime.java
@@ -0,0 +1,13 @@
+package Core_java;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DateTime {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
